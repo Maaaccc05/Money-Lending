3a5dc10bc777ff6b4f58aef53d427dd4ec86bbdc
Maaaccc05 <mackam30@gmail.com>
2026-03-12 14:46:36 +0530

    deploying base product

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = '/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add token to requests
